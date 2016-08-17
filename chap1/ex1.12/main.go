153ed5acf53065d268ac6b990c4bad903d7c75ee
zc.hu <121660097@qq.com>
2016-08-17 15:47:26 +0800

    add ex1.12

---
+++ chap1/ex1.12/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+//generate green lines
+var palette = []color.Color{color.White, color.RGBA{0x00, 0xff, 0x00, 0xff}}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		value := r.FormValue("cycles")
+		//	fmt.Fprint(w, value)    //此行代码不能要，否则会2个不同的流冲突，导致最后进入浏览器会下载一个文件
+		if len(value) == 0 {
+			value = "5"
+		}
+		cycles, _ := strconv.Atoi(value)
+
+		fmt.Printf("cycles:=%v\n", cycles)
+		lissajous(w, float64(cycles))
+	})
+
+	log.Println("Listen at :" + ":50001")
+	log.Fatal(http.ListenAndServe(":50001", nil))
+}
+
+func lissajous(out io.Writer, cycles float64) {
+	const (
+		//	cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}

a5e002dd77c79f2ac072f02e197992fab9619d2a
zc.hu <121660097@qq.com>
2016-08-21 20:56:02 +0800

    add main.go

--- chap3/ex3.10/main.go
+++ chap3/ex3.10/main.go
@@ -7,12 +7,21 @@ import (
 
 func comma(s string) string {
 	var buf bytes.Buffer
-	if len(s) < 3 {
+	n := len(s)
+	if n < 3 {
 		return s
 	}
 
 	for i, v := range s {
-		if i%3 == 2 {
+		if n%3 == 0 && i%3 == 0 && i != 0 {
+			buf.WriteString(",")
+		}
+
+		if n%3 == 1 && i%3 == 1 {
+			buf.WriteString(",")
+		}
+
+		if n%3 == 2 && i%3 == 2 {
 			buf.WriteString(",")
 		}
 		fmt.Fprintf(&buf, "%c", v)
